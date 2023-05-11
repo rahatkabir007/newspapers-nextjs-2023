85fa1eef0dd4a7f04adf601d7485d8da0df04fcf
rahatkabir007 <rahatkabir04@gmail.com>
2023-05-11 16:52:37 +0600

    logo add

--- components/shared/Footer/Footer.tsx
+++ components/shared/Footer/Footer.tsx
@@ -11,7 +11,7 @@ const Footer: React.FC<Props> = (props) => {
     return (
         <div className='bg-nlightblue py-2 flex justify-center items-center'>
             <div>
-                <span className='text-pscdarkblue font-bold'>Copyright © 2022 - খবর সমগ্র</span>
+                <span className='text-ndarkblue font-bold'>Copyright © 2022 - খবর সমগ্র</span>
             </div>
         </div>
     )
--- components/shared/Header/Header.tsx
+++ components/shared/Header/Header.tsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import { controller } from '../../../src/state/StateController'
 import { useTheme } from 'next-themes';
 import Link from 'next/link';
+import Image from 'next/image';
 
 interface Props {
 }
@@ -13,12 +14,18 @@ const Header: React.FC<Props> = (props) => {
         <div className='w-full'>
             <div className='flex items-center justify-between bg-nlightblue'>
                 <div className='container-x font-bold text-xl bg-nlightblue cursor-pointer flex justify-between items-center font-[Poppins]text-gray-800 py-4'>
-                    <div>
-                        <Link href="/">
-                            <span className='text-3xl text-nblack mr-1 tracking-widest'>
-                                খবর সমগ্র
-                            </span>
-                        </Link>
+                    <div className='flex justify-start gap-2 items-center'>
+
+                        <div>
+                            <Image src="/favicon.ico" alt="" width={35} height={35} />
+                        </div>
+                        <div>
+                            <Link href="/">
+                                <span className='text-3xl text-ndarkblue'>
+                                    খবর সমগ্র
+                                </span>
+                            </Link>
+                        </div>
 
                     </div>
 
