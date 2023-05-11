70aa93d42aa7c4634edce4aaa767c172c052fb1f
rahatkabir007 <rahatkabir04@gmail.com>
2023-05-11 16:43:59 +0600

    new codes

--- components/pages/HomePage/RightLayout/RightLayout.tsx
+++ components/pages/HomePage/RightLayout/RightLayout.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { controller } from '../../../../src/state/StateController'
 import { Jsondata } from '../../../../src/utils/Jsondata'
 
 interface Props {
 }
 
 const RightLayout: React.FC<Props> = (props) => {
-
-    const states = useSelector(() => controller.states)
 
     return (
         <div className='w-full lg:w-[20%] flex flex-col gap-5 bg-white my-4 rounded max-h-[150px] md:max-h-[200px] lg:max-h-[85vh] overflow-scroll scrollbar font-sans'>
