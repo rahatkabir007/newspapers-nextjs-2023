b69c47e1dcb715a7e268e18441002bc837d71053
rahatkabir007 <rahatkabir04@gmail.com>
2023-05-11 20:59:26 +0600

    new ui fixes

--- components/pages/HomePage/LeftLayout/NewspaperSections/NewspaperCard/NewspaperCard.tsx
+++ components/pages/HomePage/LeftLayout/NewspaperSections/NewspaperCard/NewspaperCard.tsx
@@ -21,7 +21,7 @@ const NewspaperCard: React.FC<Props> = ({ item }) => {
             <a className='p-2' href={`//${item.url}`} target='_blank'>
                 <Image src={item.img} alt={item.title} width={150} height={150} />
             </a>
-            <div className='border-t-2 border-t-ngrey w-full text-center text-xs lg:text-sm p-[4px] font-mono'>
+            <div className='border-t-2 border-t-ngrey w-full text-center text-xs lg:text-sm p-[4px] font-mono text-ndarkblue'>
                 {item.title}
             </div>
         </div>
